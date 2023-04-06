0fd26b267083407e349c0980f38311091cbb125d
Kane <ori.lowen@gmail.com>
2023-04-06 11:12:10 -1000

    Code pusedocode for getComputerChoice function

---
+++ script.js
@@ -0,0 +1,10 @@
+//Psudo Code Outline:
+
+//This game will play against the computer
+
+//Begin with a function called "getComputerChoice"
+//This function will randomly return "Rock", "Paper", or "Scissors"
+    //To return "Rock", "Paper", or "Scissors" randomly, generate a random number between 1 and 3 (or 0 and 2)
+//This function will make the computers move
+
+function getComputerChoice () {}
