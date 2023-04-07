9ef541b5f71b5e224a40736628bb6b0f8072b978
Kane <ori.lowen@gmail.com>
2023-04-07 11:52:47 -1000

    Add getComputerChoice function. This function calls getRandomIntInclusive, a function that returns 1, 2, or 3, randomly

--- script.js
+++ script.js
@@ -7,4 +7,15 @@
     //To return "Rock", "Paper", or "Scissors" randomly, generate a random number between 1 and 3 (or 0 and 2)
 //This function will make the computers move
 
-function getComputerChoice () {}
+function getComputerChoice () {
+    return getRandomIntInclusive(1,3);
+}
+
+function getRandomIntInclusive(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+
+  
