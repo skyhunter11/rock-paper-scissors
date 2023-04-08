7952fab900e5563520b883859d6534cc4692c9b4
Kane <ori.lowen@gmail.com>
2023-04-07 14:18:33 -1000

    Update getComputerChoice to return string value of 'rock' 'paper' or 'scissors'. Built playRound function

--- script.js
+++ script.js
@@ -8,8 +8,44 @@
 //This function will make the computers move
 
 function getComputerChoice () {
-    return getRandomIntInclusive(1,3);
+    switch (getRandomIntInclusive(1,3)) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        default:
+            return "Scissors";
+    }
 }
+
+function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You loose. Paper beats rock";
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You loose. Scissors beats paper";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You loose. Rock beats scissors";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win. Rock beats scissors";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win. Paper beats rock";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win. Scissors beats rock";
+    }
+    else return "Its a tie";
+}
+
+const playerSelection = "rock"
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection))
 
 function getRandomIntInclusive(min, max){
     min = Math.ceil(min);
