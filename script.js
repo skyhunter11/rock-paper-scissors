b7964fbd37666fd6e963175c054824235eb85e37
Kane <ori.lowen@gmail.com>
2023-04-08 09:19:44 -1000

    Make getPlayerChoice function. Use prompt to get player choice. Move getPlayerChoice and getComputerChoice function calls into playRound function. Make game function to call play round

--- script.js
+++ script.js
@@ -5,7 +5,12 @@
 //Begin with a function called "getComputerChoice"
 //This function will randomly return "Rock", "Paper", or "Scissors"
     //To return "Rock", "Paper", or "Scissors" randomly, generate a random number between 1 and 3 (or 0 and 2)
-//This function will make the computers move
+//This function will make the computers move. It ties ties rock,paper,scissors to the random number between 1 and 3
+//Make function that called "playRound"
+    //This function takes the playerSelection and the computerSelection as parameters
+    //This function compares playerSelection to computerSelection. It returns a string declaring the winner
+//Write function called "game".
+    //Call "playRound" inside of game to play a 5 round game. After 5 rounds "game" will declares a winner and loser.
 
 function getComputerChoice () {
     switch (getRandomIntInclusive(1,3)) {
@@ -18,7 +23,15 @@ function getComputerChoice () {
     }
 }
 
-function playRound (playerSelection, computerSelection) {
+function getPlayerChoice () {
+    return prompt("Enter 'Rock', 'Paper', or 'Scissors'.");
+}
+
+function playRound () {
+
+    let playerSelection = getPlayerChoice();
+    let computerSelection = getComputerChoice();
+
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
@@ -38,20 +51,22 @@ function playRound (playerSelection, computerSelection) {
         return "You win. Paper beats rock";
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win. Scissors beats rock";
+        return "You win. Scissors beats paper";
     }
-    else return "Its a tie";
+    else if (playerSelection === computerSelection) {
+        return "It's a tie";
+    }
+    else return "Invalid input";
 }
 
-const playerSelection = "rock"
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection))
+function game() {
+    console.log(playRound());
+}
+
+game();
 
 function getRandomIntInclusive(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
-
-
-  
